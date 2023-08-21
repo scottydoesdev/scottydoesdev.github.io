1aa6b2c7cc1999b6e2f43dacd797225a3b5ab825
David Scott <david@scottydoes.dev>
2023-08-21 16:47:46 -0700

    Of course I had linter issues.

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
       <h1 className="text-2xl p-8">Hello, there.</h1>
-      <div>I'm David, and I don't really have anything for this space right now. Check back later.</div>
+      <div>{"I'm David, and I don't really have anything for this space right now. Check back later."}</div>
     </main>
   )
 }
